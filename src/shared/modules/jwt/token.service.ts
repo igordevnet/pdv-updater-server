8e215982022d47429ea4fad0debf766f7e00c900
unknown <igorsouzaalmeida2404@gmail.com>
2026-03-18 17:57:05 -0300

    Auth Flow: Patch

--- src/shared/modules/jwt/token.service.ts
+++ src/shared/modules/jwt/token.service.ts
@@ -1,12 +1,23 @@
+import * as crypto from 'crypto';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { GenerateTokenDTO } from 'src/core/auth/dtos/generate-token.dto';
 
 @Injectable()
 export class TokenService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) { }
 
   async generateAccessToken(dto: GenerateTokenDTO): Promise<string> {
+    const atSecret = this.configService.get<string>('AT_KEY');
+
+    if (!atSecret) {
+      throw new Error('Invalid atSecret env variable');
+    }
+
     return this.jwtService.signAsync(
       {
         sub: dto.userId,
@@ -14,22 +25,13 @@ export class TokenService {
         name: dto.name,
       },
       {
-        expiresIn: 60 * 15, 
-        secret: 'at-secret',
+        expiresIn: 60 * 15,
+        secret: process.env.AT_KEY,
       },
     );
   }
 
-  async generateRefreshToken(dto: GenerateTokenDTO): Promise<string> {
-    return this.jwtService.signAsync(
-      {
-        sub: dto.userId,
-        device: dto.deviceId,
-        name: dto.name,
-      },
-      {
-        secret: 'rt-secret',
-      },
-    );
+  async generateRefreshToken(): Promise<string> {
+    return crypto.randomBytes(32).toString('hex');
   }
 }
